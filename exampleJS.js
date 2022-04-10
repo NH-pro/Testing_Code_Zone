eb624de064114357ea8efc83eec4a70933eb01e8
Neil Hanson <pro.neilhanson@gmail.com>
2022-04-10 15:27:07 -0500

    alot of js code questions added.

--- exampleJS.js
+++ exampleJS.js
@@ -1,16 +1,39 @@
 let meStatus = 'alive';
 
-function myFate() {
+function myFate(){
     if (meStatus === 'alive'){
-        document.getElementById("test").innerText= "I am destroyed";
-        document.getElementById("button").style.backgroundColor="limegreen";
-        document.getElementById("button").innerText="Big Green Button";
+        document.getElementById("test").innerText = "I am destroyed";
+        document.getElementById("button").style.backgroundColor = "limegreen";
+        document.getElementById("button").innerText = "Big Green Button";
         meStatus = 'dead';
     }
     else if (meStatus === 'dead'){
-        document.getElementById("test").innerText= "I am alive";
-        document.getElementById("button").style.backgroundColor="red";
-        document.getElementById("button").innerText="Big Red Button";
+        document.getElementById("test").innerText = "I am alive";
+        document.getElementById("button").style.backgroundColor = "red";
+        document.getElementById("button").innerText ="Big Red Button";
         meStatus = 'alive';
     }
+}
+
+let favoriteColor = '';
+let favoriteAnimal = '';
+let favoriteFood = '';
+
+function yourAnswers(){
+    if (favoriteColor === ''){
+        favoriteColor = document.getElementById("textAnswer").value;
+        document.getElementById("colorAnswer").innerText = favoriteColor;
+        document.getElementById("questions").innerText = 'What is your favorite animal?';
+    }
+    else if (favoriteAnimal === ''){
+        favoriteAnimal = document.getElementById("textAnswer").value;
+        document.getElementById("animalAnswer").innerText = favoriteAnimal;
+        document.getElementById("questions").innerText = 'What is your favorite food?';
+    }
+    else if (favoriteFood === ''){
+        favoriteFood = document.getElementById("textAnswer").value;
+        document.getElementById("foodAnswer").innerText = favoriteFood;
+        document.getElementById("questions").innerText = 'Nice to meet you!';
+    }
+
 }
