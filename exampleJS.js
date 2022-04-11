86733f06c6e9b5af75757ad0479a4e335bce6205
Neil Hanson <pro.neilhanson@gmail.com>
2022-04-11 10:15:04 -0500

    added clear input field into js

--- exampleJS.js
+++ exampleJS.js
@@ -24,16 +24,19 @@ function yourAnswers(){
         favoriteColor = document.getElementById("textAnswer").value;
         document.getElementById("colorAnswer").innerText = favoriteColor;
         document.getElementById("questions").innerText = 'What is your favorite animal?';
+        document.getElementById("textAnswer").value = '';
     }
     else if (favoriteAnimal === ''){
         favoriteAnimal = document.getElementById("textAnswer").value;
         document.getElementById("animalAnswer").innerText = favoriteAnimal;
         document.getElementById("questions").innerText = 'What is your favorite food?';
+        document.getElementById("textAnswer").value = '';
     }
     else if (favoriteFood === ''){
         favoriteFood = document.getElementById("textAnswer").value;
         document.getElementById("foodAnswer").innerText = favoriteFood;
         document.getElementById("questions").innerText = 'Those are cool facts. It is nice to meet you! If you would like to reset your answers, click "Submit Answers" again.';
+        document.getElementById("textAnswer").value = '';
     }
     else if (favoriteColor != '' && favoriteAnimal != '' && favoriteFood != ''){
         favoriteColor = '';
@@ -43,5 +46,6 @@ function yourAnswers(){
         document.getElementById("animalAnswer").innerText = 'favorite animal';
         document.getElementById("foodAnswer").innerText = 'favorite food';
         document.getElementById("questions").innerText = 'Tell me about yourself. What is your favorite color?';
+        document.getElementById("textAnswer").value = '';
     }
 }
