ae3d7f3b03d933d4e824f7ff775daad1837c3d20
Neil Hanson <pro.neilhanson@gmail.com>
2022-04-10 19:21:00 -0500

    changed some words

--- exampleJS.js
+++ exampleJS.js
@@ -2,13 +2,13 @@ let meStatus = 'alive';
 
 function myFate(){
     if (meStatus === 'alive'){
-        document.getElementById("test").innerText = "I am destroyed";
+        document.getElementById("test").innerText = "KABOOM!";
         document.getElementById("button").style.backgroundColor = "limegreen";
         document.getElementById("button").innerText = "Big Green Button";
         meStatus = 'dead';
     }
     else if (meStatus === 'dead'){
-        document.getElementById("test").innerText = "I am alive";
+        document.getElementById("test").innerText = "No Kaboom";
         document.getElementById("button").style.backgroundColor = "red";
         document.getElementById("button").innerText ="Big Red Button";
         meStatus = 'alive';
